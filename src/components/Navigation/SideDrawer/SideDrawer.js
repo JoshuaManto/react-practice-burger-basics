ca6aaab01f604e4cef71c7e066414e1d1b868405
JoshuaManto <joshuamanto@yahoo.com>
2018-12-08 16:09:41 -0800

    Added SideDrawer Toggler and the corresponding handler in Layout.js

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,23 +3,32 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import styles from './SideDrawer.module.css';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 const sideDrawer = props => {
   // animation and stuff
+  let attachedStyles = [styles.SideDrawer, styles.Close];
+  if (props.open) {
+    attachedStyles = [styles.SideDrawer, styles.Open];
+  }
 
   return (
-    <div className={styles.SideDrawer}>
-      {/* First way to dynamically set height (check Toolbar.js for explanation) */}
-      {/* <Logo newHeight="11%" /> */}
+    <Aux>
+      <BackDrop show={props.open} clicked={props.closed} />
+      <div className={attachedStyles.join(' ')}>
+        {/* First way to dynamically set height (check Toolbar.js for explanation) */}
+        {/* <Logo newHeight="11%" /> */}
 
-      {/* Second way */}
-      <div className={styles.Logo}>
-        <Logo />
+        {/* Second way */}
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,10 +4,12 @@ import styles from './Toolbar.module.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = props => (
   <header className={styles.Toolbar}>
-    <div>Menu</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+
     {/* 2 ways to to dynamically set height: */}
     {/* first, to put "height" as props and let Logo.js set it to override the css class settings via inline styles*/}
     {/* <Logo newHeight="80%" /> */}
